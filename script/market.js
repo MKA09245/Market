a5ef2bab7f5b570c115d63eac20fd33d9bd55c63
MKA09245 <24a_mka@liceum.ztu.edu.ua>
2025-04-22 12:48:45 +0300

    Update market.js

---
+++ script/market.js
@@ -0,0 +1,37 @@
+console.log("Перевірка підключеного файлу скриптів script.js");
+
+let itemsDiv = document.getElementById("items");
+
+// if (itemsDiv) {
+//     console.log(itemsDiv)
+//     console.log('Поле classList: ', itemsDiv.classList)
+//     console.log('Поле id: ', itemsDiv.id)
+//     console.log('Поле clientWidth: ', itemsDiv.clientWidth)
+//     console.log('Поле innerHTML: ', itemsDiv.innerHTML)
+//     itemsDiv.innerText = 'Перший Програмно доданий текст'
+//     itemsDiv.innerText += 'Другий Програмно доданий текст'
+//     itemsDiv.innerText = '<h1>Відформатований HTML код</h1>'
+// } else {
+//     console.log('Блок товарів не знайдено')
+// }
+
+if (itemsDiv) {
+  console.log(itemsDiv);
+  /*itemsDiv.innerHTML += '<div class = "item"></div>'
+    itemsDiv.innerHTML += '<div class = "item"></div>'
+    itemsDiv.innerHTML += '<div class = "item"></div>'
+    itemsDiv.innerHTML += '<div class = "item"></div>'*/
+  for (let i = 0; i < 100; i++) {
+    itemsDiv.innerHTML += '<div class = "item"></div>';
+  }
+} else {
+  console.log("Блок товарів не знайдено");
+}
+
+let itemsArray=["Газонокосарка 43","Електричний тример 110","Електрична газонокосарка 42","Акумуляторний оприскувач 12N","Газонокосарка 430",]
+
+console.log(itemsArray)
+
+for(let i = 0;i < itemsArray.length; i++){
+    console.log(itemsArray[i])
+}
